5b9a4a17c356a471db4a720cb8f912ea66646298
Jorge Eduardo Ardila <jeam@tuta.io>
2021-09-01 19:40:12 -0500

    feat: updated npm

--- tst.test.ts
+++ tst.test.ts
@@ -1,5 +1,5 @@
 import Prueba from './tst';
 
 test('5 + 6 = 11', () => {
-	expect.Prueba.sumar(5, 6).toBe(11);
+	expect(Prueba.sumar(5, 6)).toBe(11);
 });
--- tst.ts
+++ tst.ts
@@ -1,4 +1,4 @@
-export class Prueba {
+export default class Prueba {
 	static sumar (a: number, b: number) {
 		return a + b;
 	}
